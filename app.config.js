be05ae89e24d66faa966f595a20ae90b1efb7e7f
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2025-12-26 23:13:31 +0100

    feat: project setup

---
+++ app.config.js
@@ -0,0 +1,66 @@
+import 'dotenv/config';
+
+export default () => ({
+  expo: {
+    name: "ghost-tab",
+    slug: "ghost-tab",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "mahito",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      usesAppleSignIn: true,
+      supportsTablet: true,
+      bundleIdentifier: "dev.privy.mahito",
+      associatedDomains: ["webcredentials:<your-associated-domain>"],
+      infoPlist: {
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true
+        }
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "dev.privy.mahito"
+    },
+    web: {
+      favicon: "./assets/images/favicon.png",
+      bundler: "metro"
+    },
+    extra: {
+      privyAppId: process.env.PRIVY_APP_ID,
+      privyClientId: process.env.PRIVY_CLIENT_ID,
+      passkeyAssociatedDomain: "https://<your-associated-domain>"
+    },
+    plugins: [
+      "expo-router",
+      "expo-secure-store",
+      "expo-apple-authentication",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            deploymentTarget: "17.5"
+          },
+          android: {
+            compileSdkVersion: 35
+          }
+        }
+      ],
+      "expo-font",
+      "expo-web-browser"
+    ],
+    experiments: {
+      typedRoutes: true
+    }
+  }
+})
