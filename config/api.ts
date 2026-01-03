b310a6fc21cc41df636eaaa53f111118a66b8b37
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2026-01-03 23:45:11 +0100

    feat: backend API configuration

---
+++ config/api.ts
@@ -0,0 +1,8 @@
+export const API_CONFIG = {
+  AUTH_SERVICE: 'http://localhost:3001',
+  USER_SERVICE: 'http://localhost:3002',
+  TAB_SERVICE: 'http://localhost:3003',
+  NOTIFICATION_SERVICE: 'http://localhost:3004',
+  CHAT_SERVICE: 'http://localhost:3005',
+  WS_NOTIFICATION: 'ws://localhost:3004/notifications',
+};
